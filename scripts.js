a5b985eddcd02265f2d40f3dad8e0d2076de563f
Marty Holloway <mholloway8648@gmail.com>
2023-02-19 19:16:22 -0600

    Add code to get input from player

--- scripts.js
+++ scripts.js
@@ -6,3 +6,12 @@ function getComputerChoice() {
 }
 
 let computerSelection = getComputerChoice();
+let playerSelection = prompt("Choose between rock, paper, or scissors.")
+
+if (playerSelection === "rock") {
+    alert("You chose rock!");
+} else if (playerSelection === "paper") {
+    alert("You chose paper!");
+} else if (playerSelection === "scissors") {
+    alert("You chose scissors!")
+} else alert("Invalid selection");
