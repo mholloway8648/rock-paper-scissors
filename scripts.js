3af41ca1c87c538d3a6f99da7b9bd47080d54011
Marty Holloway <mholloway8648@gmail.com>
2023-02-20 20:20:00 -0600

    Alter function to fix computer making same selection

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let textArray = ["rock", "paper", "scissors"];
 //Gets a random selection from textArray//
 function getComputerChoice() {
     let choice = textArray[Math.floor(Math.random() * textArray.length)];
-    return choice;
+    console.log(choice);
 }
 
 let computerSelection = getComputerChoice();
