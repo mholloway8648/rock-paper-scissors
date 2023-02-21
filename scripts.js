b1bd40375ae0ff19e83fd75d6040a585632cf1fa
Marty Holloway <mholloway8648@gmail.com>
2023-02-20 20:16:03 -0600

    Add function to play one round of the game

--- scripts.js
+++ scripts.js
@@ -6,8 +6,8 @@ function getComputerChoice() {
 }
 
 let computerSelection = getComputerChoice();
+//Gets player input//
 let playerSelection = prompt("Choose between rock, paper, or scissors.")
-
 if (playerSelection === "rock") {
     alert("You chose rock!");
 } else if (playerSelection === "paper") {
@@ -15,3 +15,22 @@ if (playerSelection === "rock") {
 } else if (playerSelection === "scissors") {
     alert("You chose scissors!")
 } else alert("Invalid selection");
+
+//Plays a round using the player's input and computer's input//
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        result = "It's a tie!";
+        alert("It's a tie!");
+        return result;
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            result = "Computer won!";
+            alert("The computer won!");
+            return result;
+        } else
+             result = "Player won";
+             alert("The player won!");
+             return result;     
+    }  
+}
+
