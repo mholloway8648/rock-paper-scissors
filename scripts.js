5f0ee64e3711a9ab729eebd0ed9a3fbd05e71db4
Marty Holloway <mholloway8648@gmail.com>
2023-02-21 19:42:40 -0600

    Undo last change on line 5

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let textArray = ["rock", "paper", "scissors"];
 //Gets a random selection from textArray//
 function getComputerChoice() {
     let choice = textArray[Math.floor(Math.random() * textArray.length)];
-    console.log(choice);
+    return choice;
 }
 
 let computerSelection = getComputerChoice();
@@ -33,4 +33,5 @@ function playRound(playerSelection, computerSelection) {
              return result;     
     }  
 }
+
 
